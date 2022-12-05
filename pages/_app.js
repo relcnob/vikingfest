0b1577830dd9e7fae360449f0bf6f33904250801
Caroline Thostrup <caro342h@stud.kea.dk>
2022-12-05 22:40:56 +0100

    add footer to all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import "../styles/globals.css";
-import Head from "next/head";
+import Footer from "../components/Footer/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
 
 {
-  /* <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&family=Josefin+Sans:wght@700&display=swap" rel="stylesheet"></link> */
 }
