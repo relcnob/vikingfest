2cbc530f9b9af5378bd731b91f090b41582466ba
Fryderyk Boncler <f.relcnob@gmail.com>
2022-12-06 16:13:00 +0100

    Added Anchor component Added navbar component Added responsivity Added test page fb.js

---
+++ components/navbar/NavBar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Image from "next/image";
+import Anchor from "../Anchor";
+import styles from "./NavBar.module.css";
+import { useState } from "react";
+
+function NavBar() {
+  const [ToggleMenu, setToggleMenu] = useState("");
+  const [BurgerState, setBurgerState] = useState(styles.burger);
+  function changeMenuState() {
+    if (ToggleMenu === "") {
+      setToggleMenu(styles.show);
+      setBurgerState(styles.cross);
+    } else {
+      setToggleMenu("");
+      setBurgerState(styles.burger);
+    }
+  }
+
+  return (
+    <header className={ToggleMenu}>
+      <nav className={styles.nav}>
+        <Anchor href="/">
+          <Image alt="logotype" src=""></Image>
+        </Anchor>
+        <div className={styles.mobileNav}>
+          <ul className={styles.navWrapper}>
+            <li>
+              <Anchor href="/Bands" className={styles.link}>
+                Bands
+              </Anchor>
+            </li>
+            <li>
+              <Anchor href="/About" className={styles.link}>
+                About
+              </Anchor>
+            </li>
+            <li>
+              <Anchor href="/ScheduleApp" className={styles.link}>
+                Schedule App
+              </Anchor>
+            </li>
+          </ul>
+          <Anchor className={styles.button} href="/OrderTickets">
+            Order Tickets
+          </Anchor>
+        </div>
+
+        <div className={BurgerState} onClick={changeMenuState}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
---
+++ pages/fb.js
@@ -0,0 +1,8 @@
+import React from "react";
+import NavBar from "../components/navbar/NavBar";
+
+function testpage() {
+  return <NavBar />;
+}
+
+export default testpage;
