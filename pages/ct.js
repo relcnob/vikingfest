1b6cf1f6babda64f0342db5f04c240282d10bbcc
Caroline Thostrup <caro342h@stud.kea.dk>
2022-12-08 22:05:16 +0100

    added components to see overview

--- pages/ct.js
+++ pages/ct.js
@@ -1,12 +1,17 @@
 import React from "react";
 import InformationBar from "../components/informationBar/InformationBar";
 import Sponsors from "../components/sponsors/Sponsors";
-
+import UpcomingEvents from "../components/upcoming-events/UpcomingEvents";
+import Hero from "../components/hero/Hero";
+import ArtistsOverview from "../components/artists-overview/ArtistsOverview";
 function testpage() {
   return (
     <>
-      <Sponsors />
+      <Hero />
       <InformationBar />
+      <ArtistsOverview />
+      <Sponsors />
+      <UpcomingEvents />
     </>
   );
 }
