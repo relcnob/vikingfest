4e2b11c47998ea8ca13e3e84a1772fc388748544
Caroline Thostrup <caro342h@stud.kea.dk>
2022-12-10 23:43:45 +0100

    band view structure

--- components/band-view/BandView.jsx
+++ components/band-view/BandView.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import BandAbout from "./band-about/BandAbout";
+import BandSchedule from "./band-schedule/BandSchedule";
 import styles from "./BandView.module.css";
 function BandView() {
   return (
-    <div class={styles.about}>
-      <BandAbout
-        name="SON & SONS"
-        genre="alternative rock"
-        id="1"
-        image="img"
-        description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, assumenda. Quod in, numquam tenetur voluptatibus excepturi et consequuntur, quis odit tempora illo quasi vero voluptatum, explicabo iure qui deserunt nihil?
-Quibusdam necessitatibus ea quis doloremque dolore, autem illo consequatur soluta delectus temporibus, voluptates, pariatur ipsum quisquam eos?"
-        members="jacob, klaus, peter, jonas"
-        schedule=""
-      />
+    <div className={styles.bandView}>
+      <div className="container">
+        <div className={styles.top}>
+          <div className={styles.image}></div>
+          <div class={styles.about}>
+            <BandAbout
+              name="SON & SONS"
+              genre="alternative rock"
+              id="1"
+              image="img"
+              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, assumenda. Quod in, numquam tenetur voluptatibus excepturi et consequuntur, quis odit tempora illo quasi vero voluptatum, explicabo iure qui deserunt nihil?
+              Quibusdam necessitatibus ea quis doloremque dolore, autem illo consequatur soluta delectus temporibus, voluptates, pariatur ipsum quisquam eos?"
+              members="jacob klauristsen, klaus petersen, peter jones, jonas jacobsen "
+              schedule=""
+            />
+          </div>
+        </div>
+        <div className="bottom">
+          <BandSchedule />
+          {/* <CampingMap/> */}
+        </div>
+      </div>
     </div>
   );
 }
