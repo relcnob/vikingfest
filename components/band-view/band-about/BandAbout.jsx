811bdb2b0927a45a176da55696c462bc1b45d839
Caroline Thostrup <caro342h@stud.kea.dk>
2022-12-10 23:43:33 +0100

    band about section text styled

--- components/band-view/band-about/BandAbout.jsx
+++ components/band-view/band-about/BandAbout.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import styles from "./BandAbout.module.css";
 
 function BandAbout(props) {
   return (
-    <article>
-      <h1>{props.name}</h1>
+    <article className={styles.article}>
+      <section>
+        <h1>{props.name}</h1>
+        <h2>{props.genre}</h2>
+        <p>Members: {props.members}</p>
+      </section>
+      <section>
+        <h2>About</h2>
+        <p>{props.description}</p>
+      </section>
     </article>
   );
 }
