82ae84e5ad2f737ff154d973df25004a6e40a1e6
Caroline Thostrup <caro342h@stud.kea.dk>
2022-12-14 12:17:23 +0100

    order tickets page created

---
+++ pages/OrderTickets.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PurchaseFlow from "../components/purchase-flow/PurchaseFlow";
+
+function orderTickets() {
+  return (
+    <>
+      <PurchaseFlow />
+    </>
+  );
+}
+
+export default orderTickets;
