ea63ad305e3d16e842bc926afa7e82e956a308b6
Fryderyk Boncler <f.relcnob@gmail.com>
2022-12-15 15:54:27 +0100

    added band fetching through slug fixed some styling

--- components/band-view/BandView.jsx
+++ components/band-view/BandView.jsx
@@ -5,25 +5,42 @@ import BandSchedule from "./band-schedule/BandSchedule";
 import styles from "./BandView.module.css";
 import ornament from "../../public/images/ornament.svg";
 import placeholderImage from "../../public/images/placeholderBand.png";
-function BandView() {
+function BandView(props) {
   return (
     <div className={styles.bandView}>
       <div className="container">
         <div className={styles.top}>
           <div className={styles.image}>
-            <Image className={styles.image} src={placeholderImage} alt={"band name"} />
-            <Image className={styles.ornamentTopRight} src={ornament} alt={"ornament"} />
-            <Image className={styles.ornamentBottomLeft} src={ornament} alt={"ornament"} />
+            <Image
+              className={styles.image}
+              src={
+                props.data.logo.includes("http")
+                  ? props.data.logo
+                  : `http://localhost:8080/logos/${props.data.logo}`
+              }
+              alt={"band name"}
+              width="768"
+              height="768"
+            />
+            <Image
+              className={styles.ornamentTopRight}
+              src={ornament}
+              alt={"ornament"}
+            />
+            <Image
+              className={styles.ornamentBottomLeft}
+              src={ornament}
+              alt={"ornament"}
+            />
           </div>
           <div class={styles.about}>
             <BandAbout
-              name="SON & SONS"
-              genre="alternative rock"
+              name={props.data.name}
+              genre={props.data.genre}
+              members={props.data.members}
               id="1"
               image="img"
-              description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis, assumenda. Quod in, numquam tenetur voluptatibus excepturi et consequuntur, quis odit tempora illo quasi vero voluptatum, explicabo iure qui deserunt nihil?
-              Quibusdam necessitatibus ea quis doloremque dolore, autem illo consequatur soluta delectus temporibus, voluptates, pariatur ipsum quisquam eos?"
-              members="jacob klauristsen, klaus petersen, peter jones, jonas jacobsen "
+              description={props.data.bio}
               schedule=""
             />
           </div>
--- pages/bands/[slug].js
+++ pages/bands/[slug].js
@@ -1,10 +1,13 @@
-function bandPage() {
-  return <div>[slug]</div>;
+import BandView from "../../components/band-view/BandView.jsx";
+
+function bandPage(props) {
+  console.log(props.data);
+  return <BandView data={props.data}></BandView>;
 }
 
 export async function getServerSideProps(context) {
   const slug = context.params.slug;
-  const res = awaitfetch("http://localhost:8080/bands");
+  const res = await fetch("http://localhost:8080/bands");
 
   if (res.status !== 200) {
     return {
