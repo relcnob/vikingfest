3a067ff3c2d155210bb5eedfa899dcf9c7cc578f
Fryderyk Boncler <f.relcnob@gmail.com>
2022-12-16 17:33:40 +0100

    added event cards content added proper artists

--- components/artists-overview/ArtistsOverview.jsx
+++ components/artists-overview/ArtistsOverview.jsx
@@ -1,24 +1,50 @@
 import React from "react";
 import styles from "./ArtistsOverview.module.css";
+import Anchor from "../Anchor";
 function ArtistsOverview() {
   return (
     <section className={styles.artist_list}>
       <div className="container">
         <h2 className={styles.heading}>Artists</h2>
         <ul className={styles.list}>
-          <li>Son & Sons</li>
-          <li>Daughters</li>
-          <li>Uncles</li>
-          <li>Aunts</li>
-          <li>Fathers & mothers</li>
-          <li>Nephews</li>
+          <li>
+            <Anchor href="/bands/a-perfect-circle">A Perfect Circle</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/tool">Tool</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/terminalist">Terminalist</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/led-zeppelin">Led Zeppelin</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/the-beatles">The Beatles</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/pink-floyd">Pink Floyd</Anchor>
+          </li>
         </ul>
         <ul class={styles["small_list"]}>
-          <li>Cousins</li>
-          <li>Grandparents</li>
-          <li>The godparents</li>
-          <li>Cousins</li>
-          <li>Grandparents</li>
+          <li>
+            <Anchor href="/bands/jenkins-and-sons">Jenkins And Sons</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/dibbert-llc">Dibbert LLC</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/price-ortiz">Price - Ortiz</Anchor>
+          </li>
+          <li>
+            <Anchor href="/bands/leffler-group">Leffler Group</Anchor>
+          </li>
+          <li>
+            {" "}
+            <Anchor href="/bands/schowalter-schowalter-and-padberg">
+              Schowalter, Schowalter and Padberg
+            </Anchor>
+          </li>
         </ul>
       </div>
     </section>
--- components/upcoming-events/event-card/EventCard.jsx
+++ components/upcoming-events/event-card/EventCard.jsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
 import styles from "./EventCard.module.css";
-import placeholder from "../../../public/images/placeholder-viking.webp";
 function EventCard(props) {
   return (
     <article className={styles.cardWrapper}>
-      <Image alt="Image here" src={placeholder} className={styles.img} />
+      <Image
+        alt={props.headline}
+        src={props.image}
+        className={styles.img}
+        width="512"
+        height="512"
+      />
       <div className={styles.contentWrapper}>
         <h1 className={styles.cardHeadline}>{props.headline}</h1>
         <div className={styles.eventDetails}>
           <p>{props.date}</p>
           <p>{props.time}</p>
-          <p className={`${styles[props.stage]}`}> {props.stage}</p>
+          <p className={`${styles[props.stage.toLowerCase()]}`}>
+            {props.stage}
+          </p>
         </div>
         <p className={styles.cardText}>{props.text}</p>
       </div>
