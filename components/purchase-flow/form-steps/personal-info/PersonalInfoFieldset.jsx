01d99060890951a6b32da9eba4dfc30cb53a96a3
Lucas <lucaszago.ph@gmail.com>
2023-01-12 17:57:38 +0100

    add phone input field

--- components/purchase-flow/form-steps/personal-info/PersonalInfoFieldset.jsx
+++ components/purchase-flow/form-steps/personal-info/PersonalInfoFieldset.jsx
@@ -2,6 +2,8 @@ import { useState, useRef, useContext } from "react";
 import s from "./PersonalInfo.module.css";
 import InlineError from "../../form-components/inline-error/InlineError";
 import { CartContext } from "../../../../contexts/CartContext";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 function PersonalInfoFieldset(props) {
   const cart = useContext(CartContext);
@@ -9,66 +11,32 @@ function PersonalInfoFieldset(props) {
   const email = useRef(null);
   const dob = useRef(null);
   const phone = useRef(null);
+  const [value, setValue] = useState();
 
   return (
     <>
       <fieldset>
-        <legend>
-          {props.index === 0
-            ? "Personal information"
-            : `Guest ${props.index} information`}
-        </legend>
+        <legend>{props.index === 0 ? "Personal information" : `Guest ${props.index} information`}</legend>
         <div className={s.input_grid}>
-          <label
-            htmlFor={`full_name_guest_${props.index}`}
-            className={s["full_row"]}
-          >
+          <label htmlFor={`full_name_guest_${props.index}`} className={s["full_row"]}>
             Full name (required)
-            <input
-              type="text"
-              name={`full_name_guest_${props.index}`}
-              id={`full_name_guest_${props.index}`}
-              placeholder="John Doe"
-              ref={fullName}
-            />
-            {cart.personal_errors.includes(
-              `full_name_guest_${props.index}`
-            ) && <InlineError message={"Please insert your full name"} />}
+            <input type="text" name={`full_name_guest_${props.index}`} id={`full_name_guest_${props.index}`} placeholder="John Doe" ref={fullName} />
+            {cart.personal_errors.includes(`full_name_guest_${props.index}`) && <InlineError message={"Please insert your full name"} />}
           </label>
           <label htmlFor={`email_guest_${props.index}`} className={s.full_row}>
             Email (required)
-            <input
-              type="email"
-              name={`email_guest_${props.index}`}
-              id={`email_guest_${props.index}`}
-              placeholder="JohnDoe@email.com"
-              ref={email}
-            />
-            {cart.personal_errors.includes(`email_guest_${props.index}`) && (
-              <InlineError message={"Please insert your email"} />
-            )}
+            <input type="email" name={`email_guest_${props.index}`} id={`email_guest_${props.index}`} placeholder="JohnDoe@email.com" ref={email} />
+            {cart.personal_errors.includes(`email_guest_${props.index}`) && <InlineError message={"Please insert your email"} />}
           </label>
           <label htmlFor={`dob_guest_${props.index}`}>
             Date of birth (required)
-            <input
-              type="date"
-              name={`dob_guest_${props.index}`}
-              id={`dob_guest_${props.index}`}
-              ref={dob}
-            />
-            {cart.personal_errors.includes(`dob_guest_${props.index}`) && (
-              <InlineError message={"Please insert your date of birth"} />
-            )}
+            <input type="date" name={`dob_guest_${props.index}`} id={`dob_guest_${props.index}`} ref={dob} />
+            {cart.personal_errors.includes(`dob_guest_${props.index}`) && <InlineError message={"Please insert your date of birth"} />}
           </label>
           <label htmlFor={`phone_guest_${props.index}`}>
             Phone number (optional)
-            <input
-              type="tel"
-              name={`phone_guest_${props.index}`}
-              id={`phone_guest_${props.index}`}
-              placeholder="+45 xx xx xx xx"
-              ref={phone}
-            />
+            <PhoneInput placeholder="Enter phone number" name={`phone_guest_${props.index}`} id={`phone_guest_${props.index}`} value={value} onChange={setValue} />
+            {/* <input type="tel" name={`phone_guest_${props.index}`} id={`phone_guest_${props.index}`} placeholder="+45 xx xx xx xx" ref={phone} /> */}
           </label>
         </div>
       </fieldset>
